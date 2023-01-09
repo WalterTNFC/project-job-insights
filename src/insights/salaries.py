9815d8a5ef51778a49346045e5a549a11a22d4b1
WalterTNFC <walter.tad@hotmail.com>
2023-01-08 22:39:15 -0300

    verify req 9

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -67,16 +67,20 @@ print(teste)
 
 
 def validation(job, salary):
+
     if "min_salary" not in job or "max_salary" not in job:
         raise ValueError('Job must have valid min_salary and max_salary')
+
     # Referência:
     # https://www.geeksforgeeks.org/convert-integer-to-string-in-python/
     verify_min_salary = str(job["min_salary"]).isnumeric()
     verify_max_salary = str(job["max_salary"]).isnumeric()
     if verify_min_salary is False or verify_max_salary is False:
         raise ValueError('Salaries values must be numeric type')
-    if job["min_salary"] > job["max_salary"]:
+
+    if int(job["min_salary"]) > int(job["max_salary"]):
         raise ValueError('min value canot be greater than max value')
+
     if str(salary).lstrip('-').isnumeric() is False:
         raise ValueError('salary must have only numerics values')
     return("All validations done!")
@@ -84,7 +88,7 @@ def validation(job, salary):
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
     validation(job, salary)
-    if (int(salary) in range(job["min_salary"], job["max_salary"])) is False:
+    if (int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])) is False:
         return False
     return True
 
@@ -106,4 +110,15 @@ def filter_by_salary_range(
     list
         Jobs whose salary range contains `salary`
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+
+    filtered_jobs = []
+
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered_jobs.append(job)
+        except ValueError as error:
+            print(error)
+
+    return filtered_jobs
